a929fb426fce5371c343ed972eed13c4cdb33989
flhst <2973086776@qq.com>
2024-10-22 14:33:58 +0800

    Reader.java

--- src/java/io/Reader.java
+++ src/java/io/Reader.java
@@ -46,6 +46,16 @@ package java.io;
  *
  * @author      Mark Reinhold
  * @since       JDK1.1
+ *
+ * 字符输入流，可以从中读取数据
+ *
+ * 本地 <----------- 远端
+ *
+ * 字符输入流的解释：
+ * [输入流]的含义是从远端读取数据到本地
+ * [字符]的含义是本地获取到的是字符(远端通常是数组、字节流、字符流、通道)
+ *
+ * 注：这里的字符值char
  */
 
 public abstract class Reader implements Readable, Closeable {
@@ -93,12 +103,18 @@ public abstract class Reader implements Readable, Closeable {
      * @throws NullPointerException if target is null
      * @throws java.nio.ReadOnlyBufferException if target is a read only buffer
      * @since 1.5
+     *
+     * 尝试从字符输入流中读取足量的char填满target。返回实际填充的字符数量，返回-1表示读取失败
      */
     public int read(java.nio.CharBuffer target) throws IOException {
+        // 获取缓冲区中还剩多少空间
         int len = target.remaining();
+        // 创建等大的字符数组
         char[] cbuf = new char[len];
+        // 从字符输入流中读取足量的字符填满字符数组，返回实际读到的字符数量
         int n = read(cbuf, 0, len);
         if (n > 0)
+            // 将源字符cbuf中的所有字符写入到target缓冲区
             target.put(cbuf, 0, n);
         return n;
     }
@@ -115,6 +131,9 @@ public abstract class Reader implements Readable, Closeable {
      *             been reached
      *
      * @exception  IOException  If an I/O error occurs
+     *
+     *
+     * 从偏移量0开始读取一个字符，返回读取到的字符值，返回-1表示读取失败
      */
     public int read() throws IOException {
         char cb[] = new char[1];
@@ -135,6 +154,8 @@ public abstract class Reader implements Readable, Closeable {
      *              has been reached
      *
      * @exception   IOException  If an I/O error occurs
+     *
+     * 尝试从字符输入流中读取足量的char填满cbuf，从off处开始填充。返回实际填充的字符数量，返回-1表示读取失败
      */
     public int read(char cbuf[]) throws IOException {
         return read(cbuf, 0, cbuf.length);
@@ -153,6 +174,8 @@ public abstract class Reader implements Readable, Closeable {
      *             stream has been reached
      *
      * @exception  IOException  If an I/O error occurs
+     *
+     * 尝试从字符输入流中读取len个char，并从cbuf的off处开始填充。返回实际填充的字符数量，返回-1表示读取失败
      */
     abstract public int read(char cbuf[], int off, int len) throws IOException;
 
@@ -160,7 +183,7 @@ public abstract class Reader implements Readable, Closeable {
     private static final int maxSkipBufferSize = 8192;
 
     /** Skip buffer, null until allocated */
-    private char skipBuffer[] = null;
+    private char skipBuffer[] = null; // 临时存储跳过的字符
 
     /**
      * Skips characters.  This method will block until some characters are
@@ -210,6 +233,8 @@ public abstract class Reader implements Readable, Closeable {
      * method.
      *
      * @return true if and only if this stream supports the mark operation.
+     *
+     * 判断当前输入流是否支持存档模式，默认不支持
      */
     public boolean markSupported() {
         return false;
@@ -227,6 +252,8 @@ public abstract class Reader implements Readable, Closeable {
      *
      * @exception  IOException  If the stream does not support mark(),
      *                          or if some other I/O error occurs
+     *
+     * 设置存档标记，readAheadLimit是存档区预读上限
      */
     public void mark(int readAheadLimit) throws IOException {
         throw new IOException("mark() not supported");
@@ -244,6 +271,8 @@ public abstract class Reader implements Readable, Closeable {
      *                          or if the mark has been invalidated,
      *                          or if the stream does not support reset(),
      *                          or if some other I/O error occurs
+     *
+     * 对于支持设置存档的输入流，将输入流重置到存档位置
      */
     public void reset() throws IOException {
         throw new IOException("reset() not supported");
